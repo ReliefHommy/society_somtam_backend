9d19defd4c0250befe0999d3ebe47fa994c64f85
ReliefHommy <reliefthai@gmail.com>
2026-02-05 16:13:00 +0100

    Updated whitenoise

--- config/settings.py
+++ config/settings.py
@@ -84,7 +84,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
-     "whitenoise.middleware.WhiteNoiseMiddleware",
+ 
 ]
 
 
