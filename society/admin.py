0835eb6a014ae13e5d8a9799d7e11ded248eb217
ReliefHommy <reliefthai@gmail.com>
2026-02-05 16:34:35 +0100

    Add gis_admin to admin.py

--- society/admin.py
+++ society/admin.py
@@ -1,17 +1,26 @@
 from django.contrib import admin
+from django.contrib.gis import admin as gis_admin  # <-- add this
 from .models import Location, Event, MemberProfile
 
+
 @admin.register(Location)
-class LocationAdmin(admin.ModelAdmin):
+class LocationAdmin(gis_admin.GISModelAdmin):  # <-- change ModelAdmin -> GISModelAdmin
     list_display = ("name", "category", "country_code")
     list_filter = ("category", "country_code")
     search_fields = ("name", "address")
+
+    # Optional (nice defaults for Europe view)
+    default_zoom = 5
+    default_lon = 10
+    default_lat = 50
+
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     list_display = ("title", "event_type", "start_date", "location")
     list_filter = ("event_type", "start_date")
     search_fields = ("title", "description", "location__name")
+
 
 @admin.register(MemberProfile)
 class MemberProfileAdmin(admin.ModelAdmin):
