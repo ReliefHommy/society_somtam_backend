1233541ee8b3fc8f7a0e8433a7ef3107cb361b7c
ReliefHommy <reliefthai@gmail.com>
2026-02-06 12:08:23 +0100

    Update locationAdmni list display -related_store_external_id

--- society/admin.py
+++ society/admin.py
@@ -5,7 +5,7 @@ from .models import Location, Event, MemberProfile
 
 @admin.register(Location)
 class LocationAdmin(gis_admin.GISModelAdmin):  # <-- change ModelAdmin -> GISModelAdmin
-    list_display = ("name", "category", "country_code")
+    list_display = ("name", "category", "country_code","related_store_external_id")
     list_filter = ("category", "country_code")
     search_fields = ("name", "address")
 
@@ -17,7 +17,7 @@ class LocationAdmin(gis_admin.GISModelAdmin):  # <-- change ModelAdmin -> GISMod
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ("title", "event_type", "start_date", "location")
+    list_display = ("title", "event_type", "start_date", "location","design_template_external_id")
     list_filter = ("event_type", "start_date")
     search_fields = ("title", "description", "location__name")
 
--- society/models.py
+++ society/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+
 from django.contrib.gis.db import models  # Essential for GeoDjango
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
