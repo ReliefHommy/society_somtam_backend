92bcf9d44ea5c905279e1eb54ffacbf0b0c99f25
ReliefHommy <reliefthai@gmail.com>
2026-02-16 21:26:15 +0100

    Update Type error in Location

--- society/schemas.py
+++ society/schemas.py
@@ -8,8 +8,8 @@ class LocationOut(Schema):
     id: int
     name: str
     category: str
-    address: str | None = None
-    website: str | None = None
+    address: Optional[str] = None
+    website: Optional[str] = None
     country_code: str
     related_store_external_id: str
 
@@ -20,9 +20,9 @@ class LocationOut(Schema):
 class EventOut(Schema):
     id: int
     title: str
-    sub_title_thai: str | None = None
+    sub_title_thai: Optional[str] = None
     description: str
-    description_thai: str | None = None
+    description_thai: Optional[str] = None
     banner_image: str
     event_type: str
 
